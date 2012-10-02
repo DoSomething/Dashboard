9645e59130f682353f0866ab53506170f391221d
andrew morton <drewish@katherinehouse.com>
2012-10-02 17:35:40 -0400

    Simplifying the code to parse Google calendars.

--- widgets/calendar.js
+++ widgets/calendar.js
@@ -1,17 +1,18 @@
 var config = require('../config.js').calendar;
-var  https = require('https'),
-        qs = require('qs');
+var  https = require('https');
 
 var io;
 var freebusy; // cached free busy information
 
-var   fortress_cal = "dosomething.org_2d3538363933353536323630@resource.calendar.google.com",
-    themyscira_cal = "dosomething.org_2d38363637313938363230@resource.calendar.google.com",
-       xaviers_cal = "dosomething.org_3732343239383232313533@resource.calendar.google.com",
-        ingram_cal = "dosomething.org_2d33333338323238343834@resource.calendar.google.com",
-       batcave_cal = "dosomething.org_39363139303231372d3138@resource.calendar.google.com",
-        phone1_cal = "dosomething.org_32343432363339332d3434@resource.calendar.google.com",
-        phone2_cal = "dosomething.org_32353033383832362d3834@resource.calendar.google.com";
+var cals = {
+  'fortress': 'dosomething.org_2d3538363933353536323630@resource.calendar.google.com',
+  'themyscira': 'dosomething.org_2d38363637313938363230@resource.calendar.google.com',
+  'xaviers': 'dosomething.org_3732343239383232313533@resource.calendar.google.com',
+  'ingram': 'dosomething.org_2d33333338323238343834@resource.calendar.google.com',
+  'batcave': 'dosomething.org_39363139303231372d3138@resource.calendar.google.com',
+  'phone1': 'dosomething.org_32343432363339332d3434@resource.calendar.google.com',
+  'phone2': 'dosomething.org_32353033383832362d3834@resource.calendar.google.com',
+};
 
 /* Set refresh interval. */
 refreshCalendars();
@@ -45,7 +46,7 @@ var access_token = "";
 function refreshToken(callback) {
   var previous_token = access_token;
 
-  var post_data = qs.stringify({
+  var post_data = JSON.stringify({
     'client_id': config.CLIENT_ID,
     'client_secret': config.CLIENT_SECRET,
     'refresh_token': config.REFRESH_TOKEN,
@@ -96,16 +97,6 @@ function refreshCalendars() {
 
   console.log("Fetching free/busy information from " + now + " to " + endOfDay + ".");
 
-  // construct POST data
-  var post_data = '{"timeMin":"' + now + '","timeMax": "' + endOfDay + '","items":[';
-  for(var i = 0; i < arguments.length; i++) {
-    post_data = post_data + '{"id":"' + arguments[i] + '"}';
-    if(i < arguments.length - 1) {
-      post_data += ',';
-    }
-  }
-  post_data += ']}';
-
   // HTTP request options
   var options = {
     host: 'www.googleapis.com',
@@ -117,6 +108,16 @@ function refreshCalendars() {
       'Authorization': 'Bearer ' + access_token
     }
   }
+
+  // construct POST data
+  var post_data = {
+    "timeMin": now,
+    "timeMax": endOfDay,
+    "items":[],
+  };
+  Object.keys(cals).forEach(function(name) {
+    post_data.items.push({id: cals[name]});
+  });
 
   var req = https.request(options, function(res) {
     var buffer = "", data;
@@ -131,130 +132,77 @@ function refreshCalendars() {
 
       if(data.calendars != undefined) {
         // we got good data, yay!
-        parseCalendars(data.calendars);
+        freebusy = {};
+        Object.keys(cals).forEach(function(name) {
+          var id = cals[name];
+          freebusy[name] = parseFreeBusy(data.calendars[id].busy);
+        });
+        broadcast();
       } else {
         refreshToken(function() {
-          refreshCalendars(fortress_cal, themyscira_cal, xaviers_cal, ingram_cal, batcave_cal, phone1_cal, phone2_cal);
+          refreshCalendars();
         });
       }
     });
   });
 
-  req.write(post_data);
+  req.write(JSON.stringify(post_data));
   req.end();
 }
 
-/** Parses the free busy information returned by the Google Calendar API. */
-function parseCalendars(freebusysrc) {
-
-  // fortress
-  var fortress_json = parseFreeBusy(freebusysrc[fortress_cal].busy);
-
-  // themyscira
-  var themyscira_json = parseFreeBusy(freebusysrc[themyscira_cal].busy);
-
-  // xaviers
-  var xaviers_json = parseFreeBusy(freebusysrc[xaviers_cal].busy);
-
-  // ingram
-  var ingram_json = parseFreeBusy(freebusysrc[ingram_cal].busy);
-
-  // batcave
-  var batcave_json = parseFreeBusy(freebusysrc[batcave_cal].busy);
-
-  // phone1
-  var phone1_json = parseFreeBusy(freebusysrc[phone1_cal].busy);
-
-  // phone2
-  var phone2_json = parseFreeBusy(freebusysrc[phone2_cal].busy);
-
-  freebusy = {
-    'fortress': fortress_json,
-    'themyscira': themyscira_json,
-    'xaviers': xaviers_json,
-    'ingram': ingram_json,
-    'batcave': batcave_json,
-    'phone1': phone1_json,
-    'phone2': phone2_json
-  }
-
-  broadcast();
-
-  // ------- result -------
-  // var freebusy = {
-  //   'fortress': {
-  //     'status': 'free',
-  //     'until': '2012-07-18T11:45:47-07:00'
-  //   },
-  //
-  //   'themyscira': {
-  //     'status': 'free',
-  //     'until': '2012-07-18T11:45:47-07:00'
-  //   },
-  //
-  //   //...
-  // }
-
-}
-
+// ------- results -------
+// {
+//   'status': 'free',
+//   'until': '2012-07-18T11:45:47-07:00'
+// }
+//
+// {
+//   'status': 'inuse',
+//   'until': 'tomorrow'
+// }
 function parseFreeBusy(freebusyArray) {
   if(freebusyArray.length === 0) {
     // no events for the rest of the day
-    var result = {
+    return {
       'status': 'free',
       'until': 'tomorrow'
     }
+  }
 
-    return result;
-  } else {
-    // there are events, let's check them out
-    var result;
+  // there are events, let's check them out
 
-    // first, let's see if this room is in-use or not...
-    var now = new Date();
-    var startTime = new Date(freebusyArray[0].start);
+  // first, let's see if this room is in-use or not...
+  var now = new Date();
+  var startTime = new Date(freebusyArray[0].start);
 
-    if(startTime < now) {
-      // There's an event in progress!
+  if(startTime < now) {
+    // There's an event in progress!
+    for(var i = 0; i < freebusyArray.length; i++) {
+      if(i == freebusyArray.length - 1) {
+        // Forced end of contiguous events.
+        endTime = freebusyArray[i].end;
 
-      for(var i = 0; i < freebusyArray.length; i++) {
-        if(i == freebusyArray.length - 1) {
-          // Forced end of contiguous events.
-          endTime = freebusyArray[i].end;
-
-          break;
-        }
-
-        if(freebusyArray[i].end == freebusyArray[i+1].start) {
-          // Found a contiguous block of events. Looping until it ends.
-        } else {
-          // Found the end of the contiguous events.
-          endTime = freebusyArray[i].end;
-
-          break;
-        }
+        break;
       }
 
-      result = {
-        'status': 'inuse',
-        'until': endTime
-      }
+      if(freebusyArray[i].end == freebusyArray[i+1].start) {
+        // Found a contiguous block of events. Looping until it ends.
+      } else {
+        // Found the end of the contiguous events.
+        endTime = freebusyArray[i].end;
 
-    } else {
-      result = {
-        'status': 'free',
-        'until': freebusyArray[0].start
+        break;
       }
     }
 
-    return result;
+    return {
+      'status': 'inuse',
+      'until': endTime
+    };
   }
-}
 
-
-/* ------------- Helper Methods ------------- */
-
-function setCharAt(str,index,chr) {
-    if(index > str.length-1) return str;
-    return str.substr(0,index) + chr + str.substr(index+1);
+  return {
+    'status': 'free',
+    'until': freebusyArray[0].start
+  };
 }
