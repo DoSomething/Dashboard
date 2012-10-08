cfb5af6182ea4cec92054ca1115db0905b9e0ec6
andrew morton <drewish@katherinehouse.com>
2012-10-07 21:20:42 -0400

    Chrome and Mozilla disagree on which day of the week Sunday is.

--- public/dashboard/client.js
+++ public/dashboard/client.js
@@ -205,13 +205,14 @@ $(document).ready(function() {
             monthName;
 
         switch(day) {
-            case 0: dayName = "Sunday"; break;
             case 1: dayName = "Monday"; break;
             case 2: dayName = "Tuesday"; break;
             case 3: dayName = "Wednedsay"; break;
             case 4: dayName = "Thursday"; break;
             case 5: dayName = "Friday"; break;
             case 6: dayName = "Saturday"; break;
+            case 7: // fall through here.
+            case 0: dayName = "Sunday"; break;
         }
 
         switch(month) {
